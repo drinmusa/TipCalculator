bc75b75f139f99a97daa34448172a64e51472591
Drin Musa <drinmusa99@hotmail.com>
2020-06-07 17:25:19 +0200

    Completed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var total;
 function showAlert(message,className){
  const div = document.createElement('div');
  div.className = `alert alert-${className}`;
@@ -8,7 +9,22 @@ function showAlert(message,className){
 			//Vanish in 3 seconds
 			setTimeout(()=>document.querySelector('.alert').remove(),3000);
 }
-
+function calculateTip(billAmount,serviceQuality,numOfPeople){
+	   total = (billAmount * serviceQuality) / numOfPeople;
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+}
+function clearFields(){
+	document.getElementById('amount').value = '';
+	document.getElementById('serviceQuality').selectedIndex = 0;
+	document.getElementById('people').value = '';
+}
+function showTip(){
+	document.getElementById('output').style.display = 'block';
+	document.getElementById('tip').innerHTML=`$ ${total} each`; 
+}
 
 document.getElementById('tip-form').addEventListener('submit',(e)=>{
 	e.preventDefault();
@@ -17,11 +33,14 @@ document.getElementById('tip-form').addEventListener('submit',(e)=>{
 	const numOfPeople = document.getElementById('people').value;
 
 if(billAmount === '' || serviceQuality === '0' || numOfPeople ===''){
-		//showAlert('Please fill in all the fields','danger');
-		alert('test 1');
+		showAlert('Please fill in all the fields!','danger');
+		
 	}	
 	else {
-		alert (billAmount);
+		calculateTip(billAmount,serviceQuality,numOfPeople);
+		showTip();
+		clearFields();
+
 	}
 
 
